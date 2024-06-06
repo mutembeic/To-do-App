5e319a51c2c3e75f963b0d0d8929e404329e13e8
talescript <hello.talescript@gmail.com>
2024-06-06 13:16:40 +0200

    moved todo app idea generation into the readme

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # To-do-App
+
+
+To-Do app
+ --------
+ It might be good to research efficient to-do's examples, read on the 
+ literature.
+ --------
+ A todo list would need to have a:
+ - a datastructure to hold the different tasks. I've opted for a list
+   instead of an Object. We can refractor this later on into a class.
+ - each task will be created as a dictionary, held within a list. 
+ - Each task will have:
+    - id
+    - title
+    - description: a short summary
+    - notes: a longer description
+    - creation_date
+    - due_date
+    - completed - along with an automatic end date.
+    - priority
+
--- todo.py
+++ todo.py
@@ -1,26 +1,6 @@
 import sys
 from datetime import datetime
-"""
-To-Do app
- --------
- It might be good to research efficient to-do's examples, read on the 
- literature.
- --------
- A todo list would need to have a:
- - a datastructure to hold the different tasks. I've opted for a list
-   instead of an Object. We can refractor this later on into a class.
- - each task will be created as a dictionary, held within a list. 
- - Each task will have:
-   -- id
-   -- title
-   -- description: a short summary
-   -- notes: a longer description
-   -- creation_date
-   -- due_date
-   -- completed - along with an automatic end date.
-   -- priority
 
-"""
 
 def add_task(tasks, user_id, task_id, title, description, notes, 
              due_date, priority, completed=False):
@@ -51,7 +31,6 @@ def add_task(tasks, user_id, task_id, title, description, notes,
             raise ValueError("priority should be a string")
         if not isinstance(completed, bool):
             raise ValueError("Completed should be a boolean")
-
     except ValueError as e:
         print(f"Error: {e}")
         sys.exit()
